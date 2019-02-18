831c698c88e686285f26400cc28601d9685bd7d6
RED <apache_red@mail.ru>
2019-02-18 16:46:54 +0300

    12 Initialization sequence

---
+++ src/main/java/com/redline/red/spring/impls/db/DataBase.java
@@ -0,0 +1,13 @@
+package com.redline.red.spring.impls.db;
+
+public class DataBase {
+
+
+    public DataBase() {
+        testConnection();
+    }
+
+    private void testConnection(){
+        System.out.println("test connection...");
+    }
+}
