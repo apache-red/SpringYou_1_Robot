e60fed70dd471f143adfa0185668a352d03224fb
RED <apache_red@mail.ru>
2019-02-18 15:03:43 +0300

    v0.78 Splitting XML Files

--- src/main/java/com/redline/red/spring/main/Start.java
+++ src/main/java/com/redline/red/spring/main/Start.java
@@ -6,7 +6,7 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class Start {
     public static void main(String[] args) {
-        ApplicationContext context = new ClassPathXmlApplicationContext("context.xml");
+        ApplicationContext context = new ClassPathXmlApplicationContext("all_context.xml");
         ModelT1000 t1000 = (ModelT1000) context.getBean("t1000");
      //   t1000.action();
 //
